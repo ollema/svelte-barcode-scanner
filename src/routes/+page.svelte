251f80729feb8b8fb278192f3ecc056f43e5563c
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-01-25 01:37:01 +0100

    add simple demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,100 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { DetectedBarcode } from 'barcode-detector/pure';
+
+	import { BarcodeScanner } from '$lib/index.js';
+
+	let loading = true;
+
+	let barcode: string | undefined = undefined;
+
+	const requiredNumberOfConsistentDetections = 5;
+	let lastDetectedBarcode = '';
+	let consistentDetectionCount = 0;
+
+	function handleInit() {
+		loading = false;
+	}
+
+	async function handleDetect(e: CustomEvent<{ detections: DetectedBarcode[] }>) {
+		for (const detection of e.detail.detections) {
+			const detectedBarcode = detection.rawValue;
+
+			if (detectedBarcode !== lastDetectedBarcode) {
+				lastDetectedBarcode = detectedBarcode;
+				consistentDetectionCount = 0;
+			} else {
+				consistentDetectionCount++;
+			}
+
+			if (consistentDetectionCount >= requiredNumberOfConsistentDetections) {
+				barcode = detectedBarcode;
+				lastDetectedBarcode = '';
+				consistentDetectionCount = 0;
+				break;
+			} else {
+				return;
+			}
+		}
+	}
+</script>
+
+<div class="container">
+	<BarcodeScanner
+		on:init={handleInit}
+		on:detect={handleDetect}
+		formats={['qr_code', 'ean_8', 'ean_13']}
+		ROIs={[
+			{
+				x: 0.01,
+				y: 0.01,
+				width: 0.98,
+				height: 0.98
+			},
+			{
+				x: 0.1,
+				y: 0.2,
+				width: 0.8,
+				height: 0.6
+			},
+			{
+				x: 0.025,
+				y: 0.3,
+				width: 0.95,
+				height: 0.4
+			}
+		]}
+		showROIs={true}
+		track={(detections, ctxOverlay) => {
+			for (const detection of detections) {
+				ctxOverlay.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+				ctxOverlay.lineWidth = 4;
+				ctxOverlay.strokeRect(
+					detection.boundingBox.x,
+					detection.boundingBox.y,
+					detection.boundingBox.width,
+					detection.boundingBox.height
+				);
+			}
+		}}
+	>
+		{#if loading}
+			<div class="loading">Loading...</div>
+		{/if}
+	</BarcodeScanner>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		max-width: 384px;
+		margin: 0 auto;
+		aspect-ratio: 1;
+	}
+
+	.loading {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
